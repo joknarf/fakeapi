18326d03a7cd067597c3a2ad6683cd919dcabe06
joknarf <joknarf@users.noreply.github.com>
2023-01-15 11:36:36 +0000

    http server fix

--- fakeapi/fakeapi.py
+++ fakeapi/fakeapi.py
@@ -32,8 +32,8 @@ import sys
 from copy import copy
 from urllib.parse import urlencode, urlparse, unquote_plus #, parse_qs, quote
 from unittest.mock import MagicMock, patch
+from http.server import HTTPServer, BaseHTTPRequestHandler
 from requests.utils import requote_uri
-from .server import FakeAPIServer, FakeAPIHTTPHandler
 
 UrlConfigHelperBase = object
 
@@ -208,3 +208,40 @@ class MockAPI:
     put = None
     patch = None
     delete = None
+
+class FakeAPIHTTPHandler(BaseHTTPRequestHandler):
+    """ Class handler for HTTP """
+
+    def _set_response(self, status_code, data):
+        """ set response """
+        self.send_response(status_code)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(str(data).encode())
+
+    def do_ALL(self):
+        """ do http calls """
+        self.log_message(f"{self.command} {self.headers['Host']}{self.path}")
+        self.log_message(f'=> {self.command} {self.server.http_prefix}{self.path}')
+        content_length = int(self.headers['Content-Length'] or 0)
+        text = self.rfile.read(content_length).decode('utf-8')
+        self.log_message(text)
+        payload = json.loads(text or 'null')
+        call = getattr(self.server.fakeapi, f'{self.command.lower()}')
+        response = call(f'{self.server.http_prefix}{self.path}', data=payload)
+        self._set_response(response.status_code, response.text)
+
+    do_GET    = do_ALL
+    do_POST   = do_ALL
+    do_PUT    = do_ALL
+    do_DELETE = do_ALL
+
+class FakeAPIServer(HTTPServer):
+    """ HTTPServer with fakeapi """
+    def __init__(self, fakeapi, http_prefix, start, *args, **kwargs):
+        """ add fakeapi property """
+        self.fakeapi = fakeapi
+        self.http_prefix = http_prefix
+        super().__init__(*args, **kwargs)
+        if start:
+            self.serve_forever()
