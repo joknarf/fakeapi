b66cabe38d0a3b7bc8c7f59071327cbf893bd93d
joknarf <joknarf@free.fr>
2023-01-17 22:38:35 +0000

    v1.0.10

--- fakeapi/_version.py
+++ fakeapi/_version.py
@@ -7,5 +7,5 @@ Provides fakeapi version information.
 
 from incremental import Version
 
-__version__ = Version("fakeapi", 1, 0, 9)
+__version__ = Version("fakeapi", 1, 0, 10)
 __all__ = ["__version__"]
